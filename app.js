b9e63dbfa1ab26ee8bf6a0b939d0054f14a5551b
bokeykr <72318205+bokeykr@users.noreply.github.com>
2022-12-28 08:10:48 +0900

    cc

--- app.js
+++ app.js
@@ -1,74 +1,53 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+// server.js
+// where your node app starts
 
-app.get("/", (req, res) => res.type('html').send(html));
-app.post("/apple", (req, res) => { 
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+// The callback route used for Android, which will send the callback parameters from Apple into the Android app.
+// This is done using a deeplink, which will cause the Chrome Custom Tab to be dismissed and providing the parameters from Apple back to the app.
+app.post("/apple", (request, response) => {
   const redirect = `intent://callback?${new URLSearchParams(
-    req.body
+    request.body
   ).toString()}#Intent;package=kr.artof.jds;scheme=signinwithapple;end`;
 
   console.log(`Redirecting to ${redirect}`);
   
-  res.redirect(307, redirect);
-}); 
-app.get("/app", (req, res) => { 
-  const redirect = `intent://callback?${new URLSearchParams(
-    req.body
-  ).toString()}#Intent;package=kr.artof.jds;scheme=signinwithapple;end`;
+  response.redirect(307, redirect);
+});
 
-  //console.log(`Redirecting to ${redirect}`);
-  res.send(redirect);
-  //res.redirect(307, redirect);
-}); 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+// Endpoint for the app to login or register with the `code` obtained during Sign in with Apple
+//
+// Use this endpoint to exchange the code (which must be validated with Apple within 5 minutes) for a session in your system
+app.get("/app", async (request, response) => {
+    const redirect = `intent://callback?${new URLSearchParams(
+      request.body
+    ).toString()}#Intent;package=${
+      process.env.ANDROID_PACKAGE_IDENTIFIER
+    };scheme=signinwithapple;end`;
+  
+    console.log(`Redirecting to ${redirect}`);
+    
+    response.json(redirect);
+  
+  
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
